9db9203fe77c44b8eaad78da6d473e72650dc919
Bill Hoff <whoff@mines.edu>
2019-10-21 15:28:12 -0600

    Put messages in array

--- src/TheMotivator.java
+++ src/TheMotivator.java
@@ -1,17 +1,24 @@
 
 public class TheMotivator {
+	private String[] messages = {
+		"You're awesome", "That's great", "That's good ", "Well, what can I say?"
+	};
+	
 	public void feedback(int score) {
 		if (score > 95)
-			System.out.println("You're awesome");
+			System.out.println(messages[0]);
 		else if (score > 85)
-			System.out.println("That's great");
+			System.out.println(messages[1]);
 		else if (score > 55)
-			System.out.println("That's good ");
+			System.out.println(messages[2]);
 		else
-			System.out.println("Well, what can I say?");
+			System.out.println(messages[3]);
 	}
 	public static void main(String[] args) {
 		TheMotivator tm = new TheMotivator();
+		tm.feedback(96);
+		tm.feedback(85);
 		tm.feedback(60);
+		tm.feedback(40);
 	}
 }
