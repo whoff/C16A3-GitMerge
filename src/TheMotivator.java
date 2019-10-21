ed3409628b712bad73b328f68c104998aa7f9004
Bill Hoff <whoff@mines.edu>
2019-10-21 15:47:23 -0600

    Put scores in an array

--- src/TheMotivator.java
+++ src/TheMotivator.java
@@ -3,15 +3,17 @@ public class TheMotivator {
 	private String[] messages = {
 		"you're awesome!", "that's great!", "that's good!", "well, what can I say?"
 	};
+	
+	private int[] scores = {95, 85, 55 };
 
 	
 	public void feedback(int score) {
 		System.out.print("With a score of " + score + ", ");
-		if (score > 95)
+		if (score > scores[0])
 			System.out.println(messages[0]);
-		else if (score > 85)
+		else if (score > scores[1])
 			System.out.println(messages[1]);
-		else if (score > 55)
+		else if (score > scores[2])
 			System.out.println(messages[2]);
 		else
 			System.out.println(messages[3]);
